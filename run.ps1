979b0288f7ed88d440cd1237470a13469f95b35d
Aislan Michel Moreira Freitas <aislan.michel92@gmail.com>
2022-05-30 14:45:14 -0300

    minor fixes

--- run.ps1
+++ run.ps1
@@ -11,7 +11,7 @@ if($path -eq $null -or $path -eq "" -or $path -eq " ") {
     mkdir $solutionName
 
     Set-Location $solutionName
-}else{
+}else {
     Set-Location $path
 
     mkdir $solutionName
@@ -24,7 +24,6 @@ git init
 dotnet new sln -n $solutionName
 
 mkdir src
-
 Set-Location src
 
 dotnet new $applicationType -n $applicationName
@@ -32,12 +31,11 @@ dotnet new $applicationType -n $applicationName
 Set-Location ..
 
 $csprojFilePath = "src/" + $applicationName + "/" + $applicationName + ".csproj"
-
 dotnet sln add $csprojFilePath
 
 git add .
-
 git commit -m 'create solution'
 
 write-host "`r`npress any key to continue.."
 [Console]::ReadKey()
+
