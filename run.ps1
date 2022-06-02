12a25c3f462a58bb5202fe5e4970ee33d21f788a
Aislan Michel Moreira Freitas <aislan.michel92@gmail.com>
2022-06-02 13:12:18 -0300

    add try/catch

--- run.ps1
+++ run.ps1
@@ -9,42 +9,49 @@ param(
     [switch]$gitignore
 )
 
-if(![string]::IsNullOrWhiteSpace($path)) {
-    Set-Location $path
+try {
+    if(![string]::IsNullOrWhiteSpace($path)) {
+        Set-Location $path
+    }
+    
+    mkdir $solutionName
+    
+    Set-Location $solutionName
+    
+    git init
+    
+    dotnet new sln -n $solutionName
+    
+    mkdir src
+    
+    if($sqlFolder.IsPresent){
+        mkdir sql
+    }
+    
+    Set-Location src
+    
+    dotnet new $applicationType -n $applicationName
+    
+    Set-Location ..
+    
+    $csprojFilePath = "src/" + $applicationName + "/" + $applicationName + ".csproj"
+    dotnet sln add $csprojFilePath
+    
+    if($gitignore.IsPresent){
+        git rm -r --cached .
+    
+        dotnet new gitignore
+    }
+    
+    git add .
+    git commit -m 'create solution'
 }
-
-mkdir $solutionName
-
-Set-Location $solutionName
-
-git init
-
-dotnet new sln -n $solutionName
-
-mkdir src
-
-if($sqlFolder.IsPresent){
-    mkdir sql
+catch {
+    Write-Host "An error occurred:"
+    Write-Host $_.ScriptStackTrace
+    #todo: log errors
 }
-
-Set-Location src
-
-dotnet new $applicationType -n $applicationName
-
-Set-Location ..
-
-$csprojFilePath = "src/" + $applicationName + "/" + $applicationName + ".csproj"
-dotnet sln add $csprojFilePath
-
-if($gitignore.IsPresent){
-    git rm -r --cached .
-
-    dotnet new gitignore
+finally {
+    write-host "`r`npress any key to continue.."
+    [Console]::ReadKey()
 }
-
-git add .
-git commit -m 'create solution'
-
-write-host "`r`npress any key to continue.."
-[Console]::ReadKey()
-
